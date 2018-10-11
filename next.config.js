5a29db1f320a6f165575b0c918fea8af7b8d37c3
Rakesh Menon <rmenon3@sapient.com>
2018-10-11 12:10:21 +0530

    Added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-module.exports = {
-  exportPathMap: function () {
-    return {
-      '/': { page: '/', query: { showMore: false } },
-      '/about': { page: '/about' }
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+
+module.exports = withBundleAnalyzer({
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../../bundles/server.html'
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html'
     }
   }
-}
+});
