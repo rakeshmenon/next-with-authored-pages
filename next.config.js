546c26cf2f54d8a9e9e342f3d83616812072ce3a
Rakesh Menon <rmenon3@sapient.com>
2018-10-11 15:49:57 +0530

    Added environment configs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,27 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const nextRuntimeDotenv = require('next-runtime-dotenv');
 
-module.exports = withBundleAnalyzer({
-  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: 'static',
-      reportFilename: '../../bundles/server.html'
-    },
-    browser: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/client.html'
-    }
-  }
+const environment = process.env.NODE_ENV || 'development';
+
+const withConfig = nextRuntimeDotenv({
+  path: `${__dirname}/config/envs/${environment}.env`,
+  public: ['PAGE_SERVICE_DOMAIN'],
+  server: []
 });
+
+module.exports = withConfig(
+  withBundleAnalyzer({
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../../bundles/server.html'
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html'
+      }
+    }
+  })
+);
